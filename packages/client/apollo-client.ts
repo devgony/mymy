1b0a25423182b032d5203d218ccce9324c2e411b
devgony <devgony@gmail.com>
2022-07-15 08:36:13 +0000

    [client]modify gql setup

--- packages/client/apollo-client.ts
+++ packages/client/apollo-client.ts
@@ -17,7 +17,8 @@ export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  // uri: 'http://localhost:4000/graphql',
+  uri: 'http://henrypb.asuscomm.com:4000/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
